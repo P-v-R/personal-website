b4f2ecc917359956b5f255fb528af809359424d7
Perryvon <Perry.j.vr@gmail.com>
2021-07-01 20:35:30 -0700

    fixed easter egg formatting in homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -17,9 +17,9 @@ const containerVariants = {
   }
 }
 function HomePage() {
-  console.log("Hello there! Thanks for checking my website\
-               out, I spent a lot of time on it and think it's\
-               a good reflection of my personality. I hope you enjoy!")
+  console.log("Hello there! Thanks for checking my website")
+  console.log("out, I spent a lot of time on it and think it's")
+  console.log("a good reflection of my personality. I hope you enjoy!")
   return (
 
     <motion.div
