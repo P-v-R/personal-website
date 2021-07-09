9f5af339691891ed32ae9823fc3f6694e4f8163d
Perryvon <Perry.j.vr@gmail.com>
2021-07-08 18:59:23 -0700

    fixed up copy on about me components.

--- src/texts/bioTexts.js
+++ src/texts/bioTexts.js
@@ -4,14 +4,15 @@ const educationText = [<li>Rithm School Full Stack Development Program</li>,
 <li>LinkedIn Learning project management certificate</li>,
 <li>Over 2,000 hours of development in 2020 & 2021! and counting!</li>]
 
-const motivationText = "Simple & beautiful design. Accessible web pages equal to all, Well documented functions, Consistency in code, \
-Great testing coverage, Improving wherever can be improved, Being apart of a team, Time spend outside and A really good meal! "
+const motivationText = "Simple & beautiful design. Accessible web pages for everyone.. Consistency in code. Great\
+ testing coverage. Consistent improvement. Being a part of a team. Time spent outside. And a really good meal!"
 
 
-const howdyText = "I’m Perry — a Web Developer, Photographer, and Graphic Designer based in Los Angeles, California. "
+const howdyText = "I’m Perry—a Web Developer, Photographer, and Graphic Designer based in Los Angeles, California."
 
-const aboutMeText = "👋 Hey there! I'm a front end focused web developer with a passion for beautiful design that \
-is accessible to all, and enjoyed by many. A team player, Lover of video games, pursuer of positive impact, and overall decent human."
+const aboutMeText = "👋 Hey there! I'm a front-end focused web developer with a passion for beautiful design that’s\
+ accessible to all, and enjoyed by many. I’m a team player, lover of video games, pursuer of positive impact, \
+ and overall decent human."
 
 const joblyText = "A React Based CRUD application for finding and applying to\
   *artificial job postings. powered with\
